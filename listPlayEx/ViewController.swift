17cf4fb2d0242958a9a57222b638c0288bb45ac6
Enchan <enchant0130@gmail.com>
2019-09-06 06:48:05 +0900

    Initial Commit

---
+++ listPlayEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  listPlayEx
+//
+//  Created by EnchantCode on 2019/09/06.
+//  Copyright © 2019 EnchantCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
